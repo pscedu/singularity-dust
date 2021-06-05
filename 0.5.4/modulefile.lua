77f93a47c630ad2dd434c87e8dab4d988e04d210
icaoberg <icaoberg@andrew.cmu.edu>
2021-06-04 22:29:25 -0400

    Create modulefile.lua

---
+++ 0.5.4/modulefile.lua
@@ -0,0 +1,46 @@
+--
+-- chalk-cli 4.1.0 modulefile
+--
+-- "URL: https://www.psc.edu/resources/software"
+-- "Category: Other"
+-- "Description: Terminal string styling done right."
+
+
+whatis("Name: dust")
+whatis("Version: 0.5.4")
+whatis("Category: Other")
+whatis("URL: https://www.psc.edu/resources/software")
+whatis("Description: du + rust = dust. Like du but more intuitive.")
+
+help([[
+dust 0.5.4
+-----------
+  
+Description
+-----------
+du + rust = dust. Like du but more intuitive.
+  
+To load the module type
+
+> module load dust/0.5.4
+
+To unload the module type
+
+> module unload dust/0.5.4
+
+Documentation
+-------------
+For help, type
+  
+> dust --help
+
+Repository
+----------
+Tools included in this module are
+* dust
+]])
+
+local package = "dust"
+local version = "0.5.4"
+local base    = pathJoin("/opt/packages",package,version)
+prepend_path("PATH", base)
